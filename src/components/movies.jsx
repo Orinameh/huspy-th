86759b708fd86d61434be7a0ca99fcbc66d27b4f
Orinameh <memefacto@yahoo.com>
2021-10-14 12:22:46 +0100

    refactor moviescontent

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -3,36 +3,40 @@ import { Empty, Movie, MovieContainer } from "../styles";
 
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
+const MovieItem = ({ movie, onStar, starredMovie }) => (
+  <Movie key={movie.id}>
+    <img
+      src={`${IMG_URL}${movie.poster_path}`}
+      alt={movie.title}
+      loading="lazy"
+    />
+    <div className="info">
+      <h3>{movie.title}</h3>
+      <span>{movie.vote_average}</span>
+    </div>
+    <div className="overview">
+      <div>
+        <h3>Overview</h3>
+        <Star onClick={onStar} starredMovie={starredMovie} movie={movie} />
+      </div>
+      {movie.overview}
+    </div>
+  </Movie>
+);
+
 const MoviesContent = ({ data, onStar, starredMovie }) => {
   if (data.length === 0) {
-    return <Empty>No movies yet</Empty>;
+    return <Empty>No movies added yet</Empty>;
   }
   return (
     <MovieContainer>
       {data?.map((movie) => {
         return (
-          <Movie key={movie.id}>
-            <img
-              src={`${IMG_URL}${movie.poster_path}`}
-              alt={movie.title}
-              loading="lazy"
-            />
-            <div className="info">
-              <h3>{movie.title}</h3>
-              <span>{movie.vote_average}</span>
-            </div>
-            <div className="overview">
-              <div>
-                <h3>Overview</h3>
-                <Star
-                  onClick={onStar}
-                  starredMovie={starredMovie}
-                  movie={movie}
-                />
-              </div>
-              {movie.overview}
-            </div>
-          </Movie>
+          <MovieItem
+            movie={movie}
+            onStar={onStar}
+            starredMovie={starredMovie}
+          />
         );
       })}
     </MovieContainer>
